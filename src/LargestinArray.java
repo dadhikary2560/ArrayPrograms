514c5dc6adb8fe41d1a1825e6986d0dc28058025
dadhikary2560 <d.adhikary2560@gmail.com>
2025-12-28 23:31:27 +0530

    program to find the largest number in an array

---
+++ src/LargestinArray.java
@@ -0,0 +1,16 @@
+
+public class LargestinArray {
+public static void main(String[] args) {
+	int arr[]= {5,4,8,6,2,7,1};
+	int largest=0;
+	
+	for(int i=0;i<arr.length;i++)
+	{
+		if(arr[i]>largest)
+		{
+			largest=arr[i];
+		}
+	}
+	System.out.println("largest number is "+largest);
+}
+}
