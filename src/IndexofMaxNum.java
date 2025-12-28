a6fd62bd1e336b6ca5489d4c05bdf467291417d7
dadhikary2560 <d.adhikary2560@gmail.com>
2025-12-28 15:07:02 +0530

    index of maximum number in an array program

--- src/IndexofMaxNum.java
+++ src/IndexofMaxNum.java
@@ -1,7 +1,7 @@
 
 public class IndexofMaxNum {
 public static void main(String[] args) {
-	int arr[]= {5,6,7,4,1,2,0};
+	int arr[]= {8,5,6,7,4,1,2,0};
 	int max=0;
 	for(int i=0;i<arr.length;i++)
 	{
