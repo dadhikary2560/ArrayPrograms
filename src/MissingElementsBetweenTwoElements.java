003ae5954cb95131db83bd1d01abb622fdc3fb1c
dadhikary2560 <d.adhikary2560@gmail.com>
2025-12-30 13:48:46 +0530

    program to find missing elements in between two elements of an array

---
+++ src/MissingElementsBetweenTwoElements.java
@@ -0,0 +1,21 @@
+public class MissingElementsBetweenTwoElements {
+public static void main(String[] args) {
+	int arr[]= {1,2,3,4,7,9};
+	
+	
+	for(int i=0;i<arr.length;i++)
+	{
+		for(int j=i+1;j<arr.length;j++)
+		if(arr[i]+1==arr[j])
+		{
+			break;
+		}
+		else
+		{
+			arr[i]=arr[i]+1;
+			System.out.print(arr[i]+",");
+		}
+		System.out.println();
+	}
+}
+}
