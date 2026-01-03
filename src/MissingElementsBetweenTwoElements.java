b19dd093e13207232af461dd08729e35f6e53e5c
dadhikary2560 <d.adhikary2560@gmail.com>
2026-01-03 23:57:07 +0530

    Program to find missing elements|Not appropriate

--- src/MissingElementsBetweenTwoElements.java
+++ src/MissingElementsBetweenTwoElements.java
@@ -1,6 +1,6 @@
 public class MissingElementsBetweenTwoElements {
 public static void main(String[] args) {
-	int arr[]= {1,2,3,4,7,9};
+	int arr[]= {1,2,3,4,7,9,10,15};
 	
 	
 	for(int i=0;i<arr.length;i++)
