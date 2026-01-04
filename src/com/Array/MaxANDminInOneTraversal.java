0d342a66aca3931d7dd4bf6d36898d7d8546a812
dadhikary2560 <d.adhikary2560@gmail.com>
2026-01-05 02:22:54 +0530

    Program to find minimum and maximum number in ONE go

--- src/com/Array/MaxANDminInOneTraversal.java
+++ src/com/Array/MaxANDminInOneTraversal.java
@@ -4,19 +4,22 @@ package com.Array;
 //Find the maximum and minimum element in a single traversal.
 public class MaxANDminInOneTraversal {
 public static void main(String[] args) {
-	int arr[]= {4,10,5,8,2};
-	int max=0;
+	int arr[]= {-100,4,10,-60,5,8,2};
+	int large=arr[0];
 	int smallest=arr[0];
-	for(int i=0;i<arr.length;i++)
+	
+	for(int i=1;i<arr.length;i++)
 	{
-		if(arr[i]<smallest)
+		if(arr[i]>large)
+		{
+			large=arr[i];
+		}
+		else if(arr[i]<smallest)
 		{
 			smallest=arr[i];
 		}
-		else
-			max=arr[i];
 	}
-	System.out.println("max value of the array: "+max);
-	System.out.println("smallest value of the array: "+smallest);
+	System.out.println("largest is "+large);
+	System.out.println("smallest is "+smallest);
 }
 }
